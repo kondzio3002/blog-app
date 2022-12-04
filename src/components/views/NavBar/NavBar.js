15f85e641f56206950f35f0209d142f052f7c2c2
Konrad Brożyna <kondzio3002@gmail.com>
2022-12-04 20:10:28 +0100

    Add NavBar

--- src/components/views/NavBar/NavBar.js
+++ src/components/views/NavBar/NavBar.js
@@ -1,8 +1,17 @@
+import { NavLink } from 'react-router-dom';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+
 const NavBar = () => {
   return (
-    <>
-    <p>NavBar</p>
-    </>
+    <Navbar bg="primary" variant="dark" expand="lg" className="mt-4 mb-4 rounded">
+      <Container>
+      <Navbar.Brand>Blog.app</Navbar.Brand>
+      <Nav>
+        <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+        <Nav.Link as={NavLink} to="/about">About</Nav.Link>
+      </Nav>
+      </Container>
+    </Navbar>
   );
 };
 
