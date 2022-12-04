726739fd7725d4bd5ba1c132c2319b9f183a900d
Konrad Brożyna <kondzio3002@gmail.com>
2022-12-04 19:51:41 +0100

    Add bootstrap and Cointainer

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import { Routes, Route } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
 import Home from './components/pages/Home/Home';
 import About from './components/pages/About/About';
 import Post from './components/pages/Post/Post';
 import PostAdd from './components/pages/PostAdd/PostAdd';
 import PostEdit from './components/pages/PostEdit/PostEdit';
 import NotFound from './components/pages/NotFound/NotFound';
+import Header from './components/views/Header/Header';
+import Footer from './components/views/Footer/Footer';
 
 function App() {
   return (
     <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/post/:id" element={<Post />} />
-        <Route path="/post/add" element={<PostAdd />} />
-        <Route path="/post/edit/:id" element={<PostEdit />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
+      <Container>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/post/:id" element={<Post />} />
+          <Route path="/post/add" element={<PostAdd />} />
+          <Route path="/post/edit/:id" element={<PostEdit />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </Container>
     </main>
   );
 }
