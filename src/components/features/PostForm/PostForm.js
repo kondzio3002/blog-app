d0349cadf7b7b69c51e8e91e99642147b8241faf
Konrad Brożyna <kondzio3002@gmail.com>
2022-12-18 22:03:08 +0100

    Add validate to form

--- src/components/features/PostForm/PostForm.js
+++ src/components/features/PostForm/PostForm.js
@@ -4,6 +4,7 @@ import ReactQuill from "react-quill";
 import 'react-quill/dist/quill.snow.css';
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useForm } from "react-hook-form";
 
 const PostForm = ({ action, actionText, ...props }) => {
 
@@ -12,36 +13,69 @@ const PostForm = ({ action, actionText, ...props }) => {
   const [content, setContent] = useState(props.content || '');
   const [publishedDate, setPublishedDate] = useState(props.publishedDate || '');
   const [author, setAuthor] = useState(props.author || '');
+  const [contentError, setContentError] = useState(false);
+  const [dateError, setDateError] = useState(false);
 
-  const handleSubmit = e => {
-    e.preventDefault();
-    action({ title, author, publishedDate, shortDescription, content });
+  const { register, handleSubmit: validate, formState: { errors } } = useForm();
+
+  const handleSubmit = () => {
+    setContentError(!content);
+    setDateError(!publishedDate);
+    if(content && publishedDate) {
+      action({ title, author, publishedDate, shortDescription, content });
+    }
   };
 
   return (
-    <Form>
+    <Form onSubmit={validate(handleSubmit)}>
       <Form.Group className="col-6 mt-3">
         <Form.Label>Title</Form.Label>
-        <Form.Control type="text" value={title} onChange={e => setTitle(e.target.value)} />        
+        <Form.Control 
+          {...register("title", { required: true, minLength: 3 })}
+          type="text" 
+          value={title} 
+          onChange={e => setTitle(e.target.value)} 
+        /> 
+        {errors.title && <small className="d-block form-text text-danger mt-2">This field is too short (min is 3)</small>}       
       </Form.Group>
       <Form.Group className="col-6 mt-3">
         <Form.Label>Author</Form.Label>
-        <Form.Control type="text"value={author} onChange={e => setAuthor(e.target.value)} />        
+        <Form.Control 
+          {...register("author", { required: true, minLength: 3 })}
+          type="text"
+          value={author} 
+          onChange={e => setAuthor(e.target.value)} 
+        /> 
+        {errors.author && <small className="d-block form-text text-danger mt-2">This field is too short (min is 3)</small>}            
       </Form.Group>
       <Form.Group className="col-6 mt-3">
         <Form.Label>Published</Form.Label>
-        {/*<Form.Control type="text" value={publishedDate} onChange={e => setPublishedDate(e.target.value)} />*/}
-        <DatePicker selected={publishedDate ? new Date(publishedDate) : new Date()} onChange={(date) => setPublishedDate(date)} />      
+        <DatePicker 
+          selected={publishedDate} 
+          onChange={(date) => setPublishedDate(date)} 
+        /> 
+        {dateError && <small className="d-block form-text text-danger mt-2">This field is required</small>}       
       </Form.Group>
       <Form.Group className="mt-3">
         <Form.Label>Short description</Form.Label>
-        <Form.Control as="textarea" rows={5} value={shortDescription} onChange={e => setShortDescription(e.target.value)} />        
+        <Form.Control 
+          {...register("shortDescription", { required: true, minLength: 20 })}
+          as="textarea" 
+          rows={5} 
+          value={shortDescription} 
+          onChange={e => setShortDescription(e.target.value)} 
+        /> 
+        {errors.shortDescription && <small className="d-block form-text text-danger mt-2">This field is too short (min is 20)</small>}        
       </Form.Group>
       <Form.Group className="mt-3">
         <Form.Label>Main content</Form.Label>
-        <ReactQuill value={content} onChange={setContent} />    
+        <ReactQuill 
+          value={content} 
+          onChange={setContent} 
+        />  
+        {contentError && <small className="d-block form-text text-danger mt-2">This field is required</small>}  
       </Form.Group>
-      <Button variant="primary" className="mt-3" onClick={handleSubmit}>{actionText}</Button>
+      <Button variant="primary" className="mt-3" type="submit">{actionText}</Button>
     </Form>
   );
 }
